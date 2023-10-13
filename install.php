e4d9e98f16bd446529734da8130c70c4a300506c
t1mt4m <tam.k@oundleschool.org.uk>
2023-10-13 15:33:58 +0100

    added even more stuff

---
+++ install.php
@@ -0,0 +1,15 @@
+<?php
+include_once("connection.php");
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblUser;
+CREATE TABLE TblUsers 
+(UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Gender VARCHAR(1) NOT NULL,
+Surname VARCHAR(20) NOT NULL,
+Forename VARCHAR(20) NOT NULL,
+Password VARCHAR(20) NOT NULL,
+House VARCHAR(20) NOT NULL,
+Year INT(2) NOT NULL,
+Role TINYINT(1))");
+$stmt->execute();
+$stmt->closeCursor();
+?>
