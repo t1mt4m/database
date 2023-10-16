ac3660e9140025d1a6c94cb8c6b7e2a86d9df2f6
t1mt4m <tam.k@oundleschool.org.uk>
2023-10-16 14:59:11 +0100

    oops put it in the wrong place

--- addusers.php
+++ addusers.php
@@ -1,3 +1,11 @@
 <?php
 echo("submitted")
+        echo $_POST["gender"]."<br>";
+        echo $_POST["forename"]."<br>";
+        echo $_POST["surname"]."<br>";
+        echo $_POST["house"]."<br>";
+        echo $_POST["year"]."<br>";
+        echo $_POST["passwd"]."<br>";
+        echo $_POST["role"]."<br>";
+        
 ?>
--- users.php
+++ users.php
@@ -24,15 +24,7 @@
 		<input type="radio" name="role" value="Admin"> Admin<br>
 		<input type="submit" value="Add User">
 	  </form> 
-      <?php
-        echo $_POST["gender"]."<br>";
-        echo $_POST["forename"]."<br>";
-        echo $_POST["surname"]."<br>";
-        echo $_POST["house"]."<br>";
-        echo $_POST["year"]."<br>";
-        echo $_POST["passwd"]."<br>";
-        echo $_POST["role"]."<br>";
-        ?>
+      
     
 </body>
 </html>
