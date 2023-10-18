0da6b39b66b2cfb8c30909d61b352af4a1bcec78
t1mt4m <tam.k@oundleschool.org.uk>
2023-10-18 11:47:51 +0100

    changed addusers

--- addusers.php
+++ addusers.php
@@ -1,4 +1,28 @@
 <?php
+include_once("connection.php")
+switch($_POST["role"]){
+	case "Pupil":
+		$role=0;
+		break;
+	case "Teacher":
+		$role=1;
+		break;
+	case "Admin":
+		$role=2;
+		break;
+}
+$stmt = $conn->prepare("INSERT INTO TblUser (UserID,Gender,Surname,Forename,Password,House,Year ,Role)VALUES (null,:gender,:surname,:forename,:password,:house,:year,:role)");
+
+$stmt->bindParam(':forename', $_POST[“forename”]);
+$stmt->bindParam(':surname', $_POST[“surname”]);
+$stmt->bindParam(':house', $_POST[“house”]);
+$stmt->bindParam(':year', $_POST[“year”]);
+$stmt->bindParam(':password', $_POST[“passwd”]);
+$stmt->bindParam(':gender', $_POST[“gender”]);
+$stmt->bindParam(':role', $role);
+$stmt->execute();
+$conn=null;
+
 echo("submitted")
         echo $_POST["gender"]."<br>";
         echo $_POST["forename"]."<br>";
