777c07d0631e96ab8f0e0b1e11f0e7093e33ad5c
t1mt4m <tam.k@oundleschool.org.uk>
2023-11-17 09:43:04 +0000

    oop

--- users.php
+++ users.php
@@ -30,7 +30,7 @@
 </html>
 
 <?php
-include_once('connection/php');
+include_once('connection.php');
  $stmt = $conn->prepare("SELECT * FROM tblusers");
  $stmt = execute();
  while ($row = $stmt->fetch(\PDO::FETCH_ASSOC))
