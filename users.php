65ed0b1c5d0a3c46b52a9357b0a0cd6d47875ba5
t1mt4m <tam.k@oundleschool.org.uk>
2023-11-17 09:36:36 +0000

    h

--- users.php
+++ users.php
@@ -28,3 +28,9 @@
     
 </body>
 </html>
+
+<?php
+include_once('connection/php');
+ %stmt = $conn->prepare("SELECT * FROM tblusers");
+ $stmt = execute();
+ while ($row = $stmt->fetch(\PDO::FETCH_ASSOC))
